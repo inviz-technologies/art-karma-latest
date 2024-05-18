b4fc32ed2d574260b42ed61397fa8d026f949869
muhammadmuneebwaseem <muneebwaseem78@gmail.com>
2024-05-18 05:34:49 +0500

    changed to production url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const API_URL = "https://art-karma-f359ddaac13a.herokuapp.com/api/v1/";
-export const API_URL = "http://localhost:8080/api/v1/";
+export const API_URL = "https://art-karma-f359ddaac13a.herokuapp.com/api/v1/";
+// export const API_URL = "http://localhost:8080/api/v1/";
